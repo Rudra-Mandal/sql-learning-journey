67537be42e7c13935ef3561a27364a26dbc4acce
Rudra Mandal <129064338+Rudra-Mandal@users.noreply.github.com>
2026-07-01 18:47:12 +0530

    Add employees table and copy table SQL commands
    Added SQL commands to create an 'employees' table and a copy table from an existing table.

--- 02-Database-Basics/Advanced_table_creation.sql
+++ 02-Database-Basics/Advanced_table_creation.sql
@@ -5,10 +5,16 @@ CREATE TEMPORARY TABLE temp_analytics (
     click_count INT
 );
 
+-- Create Table if this table not exixts: 
 CREATE TABLE IF NOT EXISTS employees (
     emp_id INT PRIMARY KEY,
     name VARCHAR(100),
     department VARCHAR(50)
 );
 
+-- A copy table from another existing Table:
+CREATE TABLE new_table_name AS
+SELECT column1, column2 
+FROM existing_table 
+WHERE condition;
 
